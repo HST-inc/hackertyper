c66bf1aa525465a0ba7d6c3cf79d20cd0746d6c8
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2022-11-29 00:42:09 +0300

    Yes 2

--- win_setup.bat
+++ win_setup.bat
@@ -1,4 +1,4 @@
-set SETUP_PATH=C:\Users\$USERNAME%\hackertyper
+set SETUP_PATH=C:\Users\%USERNAME%\hackertyper
 mkdir %SETUP_PATH%
 g++ -o %SETUP_PATH%\hackertyper main.cpp
 copy sample.c %SETUP_PATH%\sample.C
